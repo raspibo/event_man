23dccf6839e20aecc4bd37fb26549607867979e3
dcast78 <dcast78@gmail.com>
2015-02-19 19:43:26 +0100

    Modificato file di log ed aggiunte alcune indicazioni d'uso all'avvio del programma

--- barcode.py
+++ barcode.py
@@ -3,8 +3,11 @@
 #output on csv file
 #usage: python barcode.py 
 import time
-out_file = open("log_python.csv","w")
-direzione=raw_input('Entrata o uscita?')
+out_file = open("log_accessi.csv","w")
+print "I dati delle scansioni vengono visualizzati a video e salvati sul file log_accessi.csv"
+print "Per terminare il programma usa Control+C"
+print "Al momento la gestione delle operazioni si basa su input utente che indica manualmente se le letture riguardano entrata o uscita"
+direzione=raw_input('Le letture sono per Entrata(E) o Uscita(U)?')
 while 1:
         input = raw_input('Inserisci un valore: ')
         print time.strftime('%Y-%m-%d %H:%M:%S') + ";" + direzione + ";" + input
